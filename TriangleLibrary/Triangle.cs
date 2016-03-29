dd4dd69f902a588a391e00e6fe2ae917874806fe
I-Am-Hated <i.am.h8d@gmail.com>
2016-03-29 17:07:35 +0300

    Triangle

---
+++ TriangleLibrary/Triangle.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TriangleLibrary
+{
+    public class Triangle
+    {
+        public float SideA { get; set; }
+        public float SideB { get; set; }
+        public float SideC { get; set; }
+    }
+
+    public class Result
+    {
+        public float? Square { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+
+    public class TriangleOperations
+    {
+        public static Result Square(Triangle triangle)
+        {
+            float[] sides = { 
+                                triangle.SideA, 
+                                triangle.SideB, 
+                                triangle.SideC 
+                            };
+
+            if (sides.Contains(0))
+            {
+                return new Result
+                {
+                    Square = 0
+                };
+            }
+
+            if (sides.Select(s => s < 0).Contains(true))
+            {
+                return new Result
+                {
+                    Square = null,
+                    ErrorMessage = "Стороны треугольника не могут быть отрицательными"
+                };
+            }
+
+            if (sides.Select(s => s >= float.MaxValue).Contains(true))
+            {
+                return new Result
+                {
+                    Square = null,
+                    ErrorMessage = String.Format("Стороны треугольника не могут превышать значение {0}", float.MaxValue)
+                };
+            }
+
+            var hypotenuse = sides.Max();
+            var cathetus = sides.Where(s => s != hypotenuse).ToArray();
+            var cathetusA = cathetus[0];
+            var cathetusB = cathetus[1];
+
+            //Так как не происходит OverflowException, то проверим переполнение вот так:
+            if (float.IsInfinity(cathetusA * cathetusA + cathetusB * cathetusB) || float.IsInfinity(cathetusA + cathetusB) || float.IsInfinity(cathetusA * cathetusB))
+                return new Result
+                {
+                    Square = null,
+                    ErrorMessage = "Переполнение. Заданы слишком большие стороны треугольника"
+                };
+
+            if (hypotenuse > cathetusA + cathetusB)
+                return new Result
+                {
+                    Square = null,
+                    ErrorMessage = "Неверно заданы стороны треугольника"
+                };
+
+            return new Result { Square = (cathetusA * cathetusB) / 2 };
+        }
+    }
+}
---
+++ UnitTests/UnitTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using TriangleLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestZero()
+        {
+            var result = TriangleOperations.Square(new Triangle
+            {
+                SideA = 0,
+                SideB = 0,
+                SideC = 0
+            });
+
+            Assert.AreEqual(0, result.Square);
+        }
+
+        [TestMethod]
+        public void TestMinus()
+        {
+            var result = TriangleOperations.Square(new Triangle
+            {
+                SideA = -1,
+                SideB = 1,
+                SideC = 2
+            });
+
+            Assert.AreEqual("Стороны треугольника не могут быть отрицательными", result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void TestHypotenuseFail()
+        {
+            var result = TriangleOperations.Square(new Triangle
+            {
+                SideA = 22,
+                SideB = 1,
+                SideC = 2
+            });
+
+            Assert.AreEqual("Неверно заданы стороны треугольника", result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void TestSideOverflow()
+        {
+            var result = TriangleOperations.Square(new Triangle
+            {
+                SideA = float.MaxValue + 1,
+                SideB = 1,
+                SideC = 2
+            });
+
+            Assert.AreEqual(String.Format("Стороны треугольника не могут превышать значение {0}", float.MaxValue), result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void TestSquareOverflow()
+        {
+            var result = TriangleOperations.Square(new Triangle
+            {
+                SideA = 3 * (float)Math.Pow(10, 25),
+                SideB = 4 * (float)Math.Pow(10, 25),
+                SideC = 5 * (float)Math.Pow(10, 25)
+            });
+
+            Assert.AreEqual("Переполнение. Заданы слишком большие стороны треугольника", result.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void TestSuccess()
+        {
+            var result = TriangleOperations.Square(new Triangle
+            {
+                SideA = 3,
+                SideB = 4,
+                SideC = 5
+            });
+
+            Assert.AreEqual(6, result.Square);
+        }
+    }
+}
